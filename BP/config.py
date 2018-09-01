dee8c9afb3dccc4562b52072f501929f8165173b
Wu Kun <38521583+ThisIsSoMe@users.noreply.github.com>
2018-09-01 10:50:36 +0800

    Create config.py

---
+++ BP/config.py
@@ -0,0 +1,10 @@
+config={
+    "iterations":1000,
+    "breakNum":50,
+    "shuffle":True,
+    "batchsize":10,
+    "learnrate":0.2,
+    "Max Entropy":True,
+    "step opt":True,
+    "regularization":True
+}
