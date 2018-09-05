80e052f597ec9849f26bc209e9fe793bb25834c9
Wu Kun <38521583+ThisIsSoMe@users.noreply.github.com>
2018-09-05 16:45:05 +0800

    Create Config.py

---
+++ BP/One Hot Representation/Config.py
@@ -0,0 +1,10 @@
+config={
+    "iterations":1000,
+    "breakNum":50,
+    "shuffle":True,
+    "batchsize":1,
+    "learnrate":0.5,
+    "Max Entropy":True,
+    "step opt":True,
+    "regularization":True
+}
