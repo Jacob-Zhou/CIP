f14bf35cdf40200d68a74eca5c895862bf370f2c
Wu Kun <38521583+ThisIsSoMe@users.noreply.github.com>
2018-09-16 15:37:34 +0800

    Create Config.py

---
+++ BP/wordvector representation/Config.py
@@ -0,0 +1,10 @@
+config={
+    "iterations":300,
+    "breakNum":20,
+    "shuffle":True,
+    "batchsize":50,
+    "learnrate":0.5,
+    "Max Entropy":True,
+    "step opt":False,
+    "regularization":True
+}
