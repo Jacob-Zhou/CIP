d1e0f2de4e248f5d73f6d21e6990456489fd7fcb
Wu Kun <38521583+ThisIsSoMe@users.noreply.github.com>
2018-09-16 15:39:05 +0800

    Create run.py

---
+++ BP/wordvector representation/run.py
@@ -0,0 +1,17 @@
+# coding=UTF-8
+import NN_word2vec
+from Config import config
+
+iterations = config["iterations"]
+break_num=config["breakNum"]
+shuffle=config["shuffle"]
+step_opt=config["step opt"]
+batchsize=config["batchsize"]
+max_entropy=config["Max Entropy"]
+learnrate=config["learnrate"]
+regularization=config["regularization"]
+
+nn=NN_word2vec.NN()
+nn.read_data()
+nn.create_feature_space()
+nn.train(iteration=iterations,break_num=break_num,shuffle=shuffle,step_opt=step_opt,batch_size=batchsize,regularization=regularization,learnrate=learnrate,max_entropy=max_entropy)
